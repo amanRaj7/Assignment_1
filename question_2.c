ce1d72b2e42f24b5057eda400c7ac6d4d7f2c43d
amanRaj7 <92115066+amanRaj7@users.noreply.github.com>
2022-03-05 15:20:16 +0530

    Update question_2.c

--- question_2.c
+++ question_2.c
@@ -4,10 +4,13 @@ int main(void) {
 	// Assignment - 1
 	/*Q2)Write a C program to compute the area and circumference of a circle for a given radius, r. Here radius,
 r have to be taken form the user. */
+    //define pi
     float pi = 3.141592;
     float r, circumference, area;
+    //get value of radius
     printf("Enter the radius of the circle: ");
     scanf("%f", &r);
+    //compute area and circumference
     circumference = 2*pi*r;
     area = pi*r*r;
     printf("Circumference of the circle: %.3f\nArea of the circle: %.3f\n", circumference, area);
