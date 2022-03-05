1239b2928a9dccf952d2767e3eed0181b2444e26
amanRaj7 <92115066+amanRaj7@users.noreply.github.com>
2022-03-05 15:14:26 +0530

    Add files via upload

---
+++ question_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+	// Assignment - 1
+	/* Q1) Write a C program to add three numbers and print the result. Here the three numbers have to be taken
+from the user.*/
+    int a, b, c, sum;
+    printf("Enter the three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+    sum = a+b+c;
+    printf("The sum of %d, %d and %d is %d", a, b, c, sum);
+	return 0;
+}
