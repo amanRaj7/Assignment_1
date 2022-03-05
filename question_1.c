71a69bc8f133ff68e4923968f31b7bbf85b44242
amanRaj7 <92115066+amanRaj7@users.noreply.github.com>
2022-03-05 15:16:54 +0530

    Update question_1.c

--- question_1.c
+++ question_1.c
@@ -5,8 +5,10 @@ int main(void) {
 	/* Q1) Write a C program to add three numbers and print the result. Here the three numbers have to be taken
 from the user.*/
     int a, b, c, sum;
+    //get values
     printf("Enter the three numbers: ");
     scanf("%d%d%d", &a, &b, &c);
+    //compute sum
     sum = a+b+c;
     printf("The sum of %d, %d and %d is %d", a, b, c, sum);
 	return 0;
